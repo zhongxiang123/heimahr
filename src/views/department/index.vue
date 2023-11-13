735d2c7c3981a828a414aeb9d1a219e69ce56957
[zhongxiang123] <2675617908@qq.com>
2023-11-13 10:33:58 +0800

    路由组件

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      组织架构123
     </div>
   </div>
 </template>
+
 <script>
 export default {
   name: 'Department'
