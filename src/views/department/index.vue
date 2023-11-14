27924cc0a267c769f2c9c56a5a62e17b9d614357
[zhongxiang123] <2675617908@qq.com>
2023-11-14 09:46:30 +0800

    el-tree初步实现

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -3,7 +3,36 @@
     <div class="app-container">
       <!-- 组织架构123
       展示树形结构 -->
-      <el-tree default-expand-all :data="depts" :props="defaultProps" />
+      <el-tree default-expand-all :data="depts" :props="defaultProps">
+        <!-- 节点结构 -->
+        <!-- v-slot="{ node, data }" 只能作用在template -->
+        <template v-slot="{ data }">
+          <el-row
+            style="width: 100%; height: 40px"
+            type="flex"
+            justify="space-between"
+            align="middle"
+          >
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <!-- $event 实参 表示类型 -->
+              <el-dropdown>
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <!-- 下拉菜单选项 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -13,14 +42,16 @@ export default {
   name: 'Department',
   data() {
     return {
-      depts: [{
-        name: '传智教育',
-        children: [
-          { name: '总裁办' },
-          { name: '行政部' },
-          { name: '财务部' }
-        ]
-      }],
+      depts: [
+        {
+          name: '传智教育',
+          children: [
+            { name: '总裁办' },
+            { name: '行政部' },
+            { name: '财务部' }
+          ]
+        }
+      ],
       defaultProps: {
         children: 'children',
         label: 'name'
@@ -31,9 +62,8 @@ export default {
 </script>
 
 <style scoped>
-.app-container{
+.app-container {
   padding: 30px 140px;
   font-size: 14px;
 }
-
 </style>
