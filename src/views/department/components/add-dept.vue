4c5459d4d0e2e30bc640f013870f613038acc60f
[zhongxiang123] <2675617908@qq.com>
2023-11-14 11:40:24 +0800

    新增弹层基本校验

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -1,18 +1,34 @@
 <template>
   <el-dialog title="新增部门" :visible="showDialog" @close="close">
     <!-- 放置弹层内容 -->
-    <el-form label-width="120px">
+    <el-form ref="addDept" :model="formData" :rules="rules" label-width="120px">
       <el-form-item prop="name" label="部门名称">
-        <el-input placeholder="2-10个字符" style="width: 80%" size="mini" />
-      </el-form-item>
-      <el-form-item label="部门编码">
-        <el-input placeholder="2-10个字符" style="width: 80%" size="mini" />
-      </el-form-item>
-      <el-form-item label="部门负责人">
-        <el-select placeholder="请选择负责人" style="width: 80%" size="mini" />
-      </el-form-item>
-      <el-form-item label="部门介绍">
         <el-input
+          v-model="formData.name"
+          placeholder="2-10个字符"
+          style="width: 80%"
+          size="mini"
+        />
+      </el-form-item>
+      <el-form-item prop="code" label="部门编码">
+        <el-input
+          v-model="formData.code"
+          placeholder="2-10个字符"
+          style="width: 80%"
+          size="mini"
+        />
+      </el-form-item>
+      <el-form-item prop="managerId" label="部门负责人">
+        <el-select
+          v-model="formData.managerId"
+          placeholder="请选择负责人"
+          style="width: 80%"
+          size="mini"
+        />
+      </el-form-item>
+      <el-form-item prop="introduce" label="部门介绍">
+        <el-input
+          v-model="formData.introduce"
           placeholder="1-100个字符"
           type="textarea"
           size="mini"
@@ -38,6 +54,50 @@ export default {
     showDialog: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      formData: {
+        code: '', // 部门编码
+        introduce: '', // 部门介绍
+        managerId: '', // 部门负责人id
+        name: '', // 部门名称
+        pid: '' // 父级部门的id
+      },
+      rules: {
+        code: [
+          { required: true, message: '部门编码不能为空', trigger: 'blur' },
+          {
+            min: 2,
+            max: 10,
+            message: '部门编码的长度为2-10个字符',
+            trigger: 'blur'
+          }
+        ], // 部门编码
+        introduce: [
+          { required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          {
+            min: 1,
+            max: 100,
+            message: '部门介绍的长度为1-100个字符',
+            trigger: 'blur'
+          }
+        ], // 部门介绍
+        managerId: [
+          { required: true, message: '部门负责人不能为空', trigger: 'blur' }
+        ], // 部门负责人id
+        name: [
+          { required: true, message: '部门名称不能为空', trigger: 'blur' },
+          {
+            min: 2,
+            max: 10,
+            message: '部门名称的长度为2-10个字符',
+            trigger: 'blur'
+          }
+        ] // 部门名称
+        // pid: '' // 父级部门的id 不需要做校验
+      }
     }
   },
   methods: {
