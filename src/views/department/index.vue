b40f05048e5977762d09dc977a2e7312ddaddec8
[zhongxiang123] <2675617908@qq.com>
2023-11-15 10:02:15 +0800

    记录部门id

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <!-- 组织架构123
-      展示树形结构 -->
+      <!-- 展示树形结构 -->
       <el-tree :expand-on-click-node="false" default-expand-all :data="depts" :props="defaultProps">
         <!-- 节点结构 -->
         <!-- v-slot="{ node, data }" 只能作用在template -->
         <template v-slot="{ data }">
-          <el-row
-            style="width: 100%; height: 40px"
-            type="flex"
-            justify="space-between"
-            align="middle"
-          >
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
               <span class="tree-manager">{{ data.managerName }}</span>
               <!-- $event 实参 表示类型 -->
-              <el-dropdown @command="operateDept">
+              <el-dropdown @command="operateDept($event, data.id)">
                 <!-- 显示区域内容 -->
                 <span class="el-dropdown-link">
                   操作<i class="el-icon-arrow-down el-icon--right" />
@@ -35,11 +29,10 @@
       </el-tree>
     </div>
     <!-- 放置弹层 -->
-    <!-- sync表示会监听父组件的值 -->
-    <add-dept :show-dialog.sync="showDialog" />
+    <!-- 表示会接受子组件的事件  update:showDialog, 值 => 属性 -->
+    <add-dept :current-node-id="currentNodeId" :show-dialog.sync="showDialog" @updateDepartment="getDepartment" />
   </div>
 </template>
-
 <script>
 import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils'
@@ -49,11 +42,12 @@ export default {
   components: { AddDept },
   data() {
     return {
+      currentNodeId: null, // 存储当前点击的id
       showDialog: false, // 控制弹层的显示和隐藏
       depts: [], // 数据属性
       defaultProps: {
-        children: 'children',
-        label: 'name'
+        label: 'name', // 要显示的字段的名字
+        children: 'children' // 读取子节点的字段名
       }
     }
   },
@@ -61,22 +55,22 @@ export default {
     this.getDepartment() // 调用获取数据的接口
   },
   methods: {
-    // 封装好方法 后期便于利用
+    // 封装好方法
     async getDepartment() {
       const result = await getDepartment()
       this.depts = transListToTreeData(result, 0)
     },
-    operateDept(type) {
-      // alert(type)
+    // 操作部门方法
+    operateDept(type, id) {
       if (type === 'add') {
         // 添加子部门
-        this.showDialog = true// 显示弹层
+        this.showDialog = true // 显示弹层
+        this.currentNodeId = id
       }
     }
   }
 }
 </script>
-
 <style scoped>
 .app-container {
   padding: 30px 140px;
