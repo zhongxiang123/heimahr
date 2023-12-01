9d0e1931debf628626d9a5eed7ffe6508dc2a294
[zhongxiang123] <2675617908@qq.com>
2023-12-01 11:36:25 +0800

    防抖查询

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -3,11 +3,13 @@
     <div class="app-container">
       <div class="left">
         <el-input
-          style="margin-bottom: 10px"
+          v-model="queryParams.keyword"
+          style="margin-bottom:10px"
           type="text"
           prefix-icon="el-icon-search"
           size="small"
           placeholder="输入员工姓名全员搜索"
+          @input="changeValue"
         />
         <!-- 树形组件 -->
         <el-tree
@@ -88,7 +90,8 @@ export default {
       queryParams: {
         departmentId: null,
         page: 1, // 当前页码
-        pagesize: 10
+        pagesize: 10,
+        keyword: '' // 模糊搜索字段
       },
       list: []
     }
@@ -126,6 +129,16 @@ export default {
     changePage(newPage) {
       this.queryParams.page = newPage // 赋值新页码
       this.getEmployeeList() // 查询数据
+    },
+    // 输入值改变时触发
+    changeValue() {
+      // 单位时间内只执行最后一次
+      // this的实例上赋值了一个timer的属性
+      clearTimeout(this.timer) // 清理上一次的定时器
+      this.timer = setTimeout(() => {
+        this.queryParams.page = 1
+        this.getEmployeeList()
+      }, 300)
     }
   }
 }
