6c330c68b76906169dca738d3ae2b4f7549657f7
[zhongxiang123] <2675617908@qq.com>
2023-12-06 09:54:42 +0800

    级联-双向绑定

--- src/views/employee/components/select-tree.vue
+++ src/views/employee/components/select-tree.vue
@@ -5,12 +5,19 @@
     :options="treeData"
     :props="props"
     separator="-"
+    @change="changeValue"
   />
 </template>
 <script>
 import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils'
 export default {
+  props: {
+    value: {
+      type: Number, // 存储的是部门的id
+      default: null
+    }
+  },
   data() {
     return {
       treeData: [], // 赋值给 级联组件的options
@@ -26,6 +33,14 @@ export default {
   methods: {
     async getDepartment() {
       this.treeData = transListToTreeData(await getDepartment(), 0) // 将组织架构的数据 转化树形赋值给treeData
+    },
+    changeValue(list) {
+      // 取到数组的最后一次
+      if (list.length > 0) {
+        this.$emit('input', list[list.length - 1]) // 将最后一位的id取出来 传出去
+      } else {
+        this.$emit('input', null) // 如果长度为0 说明值为空
+      }
     }
   }
 }
